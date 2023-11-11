227491b30a4cee114ba33319e67f9189689cac1d
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-10 21:04:03 -0300

    Update chat functionality with new message elements

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,6 +3,12 @@ const login = document.querySelector('.login');
 const loginForm = login.querySelector('.login__form');
 const loginInput = login.querySelector('.login__input');
 
+//chat elements
+const chat = document.querySelector('.chat');
+const chatForm = chat.querySelector('.chat__form');
+const chatInput = chat.querySelector('.chat__input');
+const chatMessages = chat.querySelector('.chat__messages');
+
 const colors = [
     'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
     'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue',
@@ -22,14 +28,55 @@ const colors = [
     'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'
 ];
 
+const user = { id: "", name: "", color: "" }
+
+let websocket;
+
+const createMessageSelfElement = (content) => {
+
+    const div = document.createElement('div');
+    div.classList.add('message--self');
+    div.innerHTML = content;
+
+    return div;
+}
+
+const createMessageOtherElement = (name, color, content) => {
+
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    
+    div.classList.add('message--other');
+    span.classList.add('message--sender');
+    
+    div.appendChild(span);
+
+    span.innerHTML = name;
+    span.style.color = color;
+    div.innerHTML += content;
+
+    return div;
+
+}
+
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
 
-const user = { id: "", name: "", color: "" }
+const processMessage = ({ data }) => {
+    const { userSender, content } = JSON.parse(data);
 
-const handleSubmit = (e) => {
+    if(userSender.id === user.id) {
+        const element = createMessageSelfElement(content);
+        chatMessages.appendChild(element);
+    } else {
+        const element = createMessageOtherElement(userSender.name, userSender.color, content);
+        chatMessages.appendChild(element);
+    }
+}
+
+const handleLogin = (e) => {
     e.preventDefault();
     
     user.id = crypto.randomUUID();
@@ -37,6 +84,25 @@ const handleSubmit = (e) => {
     user.color = getRandomColor();
     
     login.style.display = 'none';
+    chat.style.display = 'flex';
+
+    websocket = new WebSocket('ws://localhost:8080');
+    websocket.onmessage = processMessage;
 }
 
-loginForm.addEventListener('submit', handleSubmit);
+const sendMessage = (e) => {
+    e.preventDefault();
+
+    const message = {
+        userSender: user,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message));
+    
+    chatInput.value = '';
+}
+
+loginForm.addEventListener('submit', handleLogin);
+
+chatForm.addEventListener('submit', sendMessage);
